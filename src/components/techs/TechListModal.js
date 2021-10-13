57f407884a8dc52711e94aa1831249e68a924fa2
kaitekelly <murphy.kaite@gmail.com>
2021-10-13 12:55:55 -0700

    redux store and provider setup

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -24,9 +24,9 @@ const TechListModal = () => {
 
     return (
         <div id="tech-list-modal" className="modal">
-            <div class="modal-content">
+            <div className="modal-content">
                 <h4>Technician List</h4>
-                <ul class="collection">
+                <ul className="collection">
                     {!loading && techs.map(tech => (
                         <TechItem tech={tech} key={tech.id}/>
                     ))}
